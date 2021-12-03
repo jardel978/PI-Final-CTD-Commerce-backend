2313de6c81e6d94aa0e4f36eb8b2462f8205398f
GasparPSousa <gasparufrj@gmail.com>
2021-12-03 16:18:31 -0300

    Adicionando http://localhost:3000/%22 no origim

--- src/main/java/com/dh/projetofinal/CTDCommerce/configuration/CorsConfiguration.java
+++ src/main/java/com/dh/projetofinal/CTDCommerce/configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
-                .allowedOrigins("")
+                .allowedOrigins("http://localhost:3000/%22")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 
