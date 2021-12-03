2268b57aeb9ac34083c9bc0f4cd411d044ec86a3
GasparPSousa <gasparufrj@gmail.com>
2021-12-03 17:12:46 -0300

    Deletando as classes CorsConfiguration e MyConfiguration

--- src/main/java/com/dh/projetofinal/CTDCommerce/configuration/WebConfig.java
+++ src/main/java/com/dh/projetofinal/CTDCommerce/configuration/WebConfig.java
@@ -1,11 +1,10 @@
 package com.dh.projetofinal.CTDCommerce.configuration;
 
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
@@ -13,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**");
     }
 }
